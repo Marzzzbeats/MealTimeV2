bef2a0963f02dc604744334f105d4a07fdd4f769
Marzzzbeats <simonmarzin@gmail.com>
2026-04-19 22:36:50 +0200

    new crud favoris

--- crud/favoris.crud.php
+++ crud/favoris.crud.php
@@ -11,6 +11,45 @@
         return $tab;
     }
 
+    function getRecettesFavFormat($conn, $user_id){
+        $res = array();
+        $fav = getRecettesFav($conn, $user_id);
+        $created = array();
+        $tab = array();
+        $current_id = $fav[0]['id_recette'];
+        $created['id_recette']=$fav[0]['id_recette'];
+        $created['owner'] = $fav[0]['owner'];
+        $created['saison'] = $fav[0]['saison'];
+        $created['price_ind'] = $fav[0]['price_ind'];
+        $created['health_ind'] = $fav[0]['health_ind'];
+        $created['titre'] = $fav[0]['titre'];
+        $created['description'] = $fav[0]['description'];
+        $created['upvote'] = $fav[0]['upvote'];
+            foreach($fav as $created_res){
+                if($created_res['id_recette'] != $current_id){
+                    $created['ing'] = $tab;
+                    $res[] = $created;
+                    $created = [];
+                    $tab = [];
+                    $current_id = $created_res['id_recette'];
+                    $created['id_recette']=$created_res['id_recette'];
+                    $created['owner'] = $created_res['owner'];
+                    $created['saison'] = $created_res['saison'];
+                    $created['price_ind'] = $created_res['price_ind'];
+                    $created['health_ind'] = $created_res['health_ind'];
+                    $created['titre'] = $created_res['titre'];
+                    $created['description'] = $created_res['description'];
+                    $created['upvote'] = $created_res['upvote'];
+                }else{
+                    $row = ['id_ingredient' => $created_res['id_ingredient'], 'quantite' => $created_res['quantite'], 'nom' => $created_res['nom']];
+                    $tab[] = $row;
+                }
+            }
+        $created['ing'] = $tab;
+        $res[] = $created;
+        return $res;
+    }
+
     function deleteRecetteFav($conn, $recette_id, $user_id){
         //Retire une recette des favoris du user
         $sql = "DELETE FROM favoris WHERE recette_id = $recette_id AND user_id = $user_id";
